443714c54ba381ca9f5b5a653bfeb17ef445dbd8
TomB <automizelife@gmail.com>
2018-06-21 22:12:34 -0700

    going towards single page webapp

---
+++ public/javascripts/graphLogic.js
@@ -0,0 +1,21 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+<link rel='stylesheet' href='/stylesheets/style.css' />
+ //make server get request to pi
+ $.get( "http://47.153.171.2:9000/gethistory", function(data) {
+
+    results = data;
+
+    var dates = [];
+    var temps = [];
+    for(var i=0; i < results.length; ++i) {
+    
+    dates.push(results[i][0]);
+    temps.push(results[i][1]);
+}
+TESTER = document.getElementById('tester');
+Plotly.plot( TESTER, [{
+x: dates,
+y: temps }], {
+margin: { t: 0 } } );
+})  
